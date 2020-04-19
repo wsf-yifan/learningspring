8c1c8420f1a011e1907ab2432e6660512b4ef157
wyifan <wyifan@vip.qq.com>
2020-04-19 21:35:11 +0800

    init project

---
+++ resourceserver/src/test/java/com/yifan/spring/resourceserver/ResourceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yifan.spring.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
